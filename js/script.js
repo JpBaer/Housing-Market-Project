11d3f905c8959cf3b8b76e06c4d50b75bcc33a6f
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-02-07 18:57:27 -0800

    Initial google maps comments

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// ----------------Beginning of Google Maps Section--------------- //
+
+
+// We will need 3 seperate Map functions
+// #1 Take users current location and display on the page upon page load
+    // User will have to have location sharing enabled
+// #2 Replace current location with the location of the searched area in map section of main page
+    //Bonus points if you can display icons for each house
+// #3 On Single House page show location of selected house and street view if applicable
+
+var GoogleAPIKey = "AIzaSyCxd2Ls7wflVthdU9GtS3jhfKlUOaMxd0U"
+// Example HTTPS Call https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_Key
+// JSON signifies to return response in JSON
+// Address components seperated by +
+
+
+
+
+
+
+
+
+
+
+// ----------------- End of Google Maps Section ---------------- //
