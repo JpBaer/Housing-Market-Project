1c09d5105e2fd9ed03eaab455c59d33290f45191
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-02-15 19:25:26 -0800

    Single house styling

--- js/singlehouse.js
+++ js/singlehouse.js
@@ -25,13 +25,14 @@ function initMap(){
 }
 
 // using local storage to pass house data to single house page
+
 document.getElementById("show-address").innerHTML = "<strong>House Address:</strong> " + localStorage.getItem("house-address");
 document.getElementById("show-price").innerHTML = "<strong>House Price:</strong> " + localStorage.getItem("house-price");
 document.getElementById("house-photo").setAttribute("src", localStorage.getItem("house-picture"));
 document.getElementById("show-list-date").innerHTML = "<strong>Listing Date:</strong> " + dayjs(localStorage.getItem("house-list-date"), 'YYYY-MM-DD').format('MMM D, YYYY');
 document.getElementById("show-prop-type").innerHTML = "<strong>Property Type:</strong> " + localStorage.getItem("house-prop-type");
 document.getElementById("show-beds").innerHTML = "<strong># of Bedrooms:</strong> " + localStorage.getItem("house-beds");
-document.getElementById("show-baths").innerHTML = "<strong># of Bathrooms:<strong> " + localStorage.getItem("house-baths");
+document.getElementById("show-baths").innerHTML = "<strong># of Bathrooms:</strong> " + localStorage.getItem("house-baths");
 document.getElementById("show-sqft").innerHTML = "<strong>Square Footage:</strong> " + localStorage.getItem("house-sqft");
 document.getElementById("show-office-name").innerHTML = "<strong>Office Name:</strong> " + localStorage.getItem("house-office-name");
 document.getElementById("show-url").innerHTML = "<strong>URL:</strong> " + localStorage.getItem("house-url");
